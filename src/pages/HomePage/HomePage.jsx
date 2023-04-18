481d2e6a099575d94d2b6448d4c96ea0f9c9510b
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-04-18 22:16:42 +0200

    adds HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,32 @@
+import Container from 'components/Container/Container';
+import s from './HomePage.module.scss';
+import { useMediaQuery } from 'react-responsive';
 
 const HomePage = () => {
+  const isTablet = useMediaQuery({ query: '(min-width: 768px)' });
 
   return (
-        <h1>Home</h1>
+    <>
+      <Container>
+        {!isTablet && (
+          <h1 className={s.homeTitle}>
+            <span className={s.homeTitleAccent}> Planner</span> for joint
+            savings for an apartment
+          </h1>
+        )}
+        {isTablet && (
+          <h1 className={s.homeTitle}>
+            <span>
+              {' '}
+              <span className={s.homeTitleAccent}> Planner</span> for joint
+            </span>
+            savings for an apartment
+          </h1>
+        )}
+
+        <div className={s.homeImg} />
+      </Container>
+    </>
   );
 };
 
