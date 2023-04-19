31a7b4d960082098d76c94aada7f667172f0ed22
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-04-19 17:54:26 +0300

    Update OwnPlanPage.jsx

--- src/pages/OwnPlanPage/OwnPlanPage.jsx
+++ src/pages/OwnPlanPage/OwnPlanPage.jsx
@@ -4,7 +4,6 @@ import PlanInputsList from 'components/PlanInputsList/PlanInputsList';
 const OwnPlanPage = () => {
   return (
     <Container>
-      <h1>OwnPlanPage</h1>
       <PlanInputsList />
     </Container>
   );
