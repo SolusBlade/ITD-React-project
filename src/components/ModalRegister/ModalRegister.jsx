1a79cb31ea60914bfb5f62ec84abebe0a5bac0a5
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-04-19 20:17:16 +0200

    adds Register/Login authOperations onSubmit

--- src/components/ModalRegister/ModalRegister.jsx
+++ src/components/ModalRegister/ModalRegister.jsx
@@ -1,10 +1,11 @@
 import UserForm from 'components/UserForm/UserForm';
 import Modal from 'components/Modal/Modal';
+import { registerUser } from 'redux/auth/authOperations';
 
 const ModalRegister = () => {
   return (
     <Modal>
-      <UserForm btnSubmit="Register" />
+      <UserForm onSubmit={registerUser} btnSubmit="Register" />
     </Modal>
   );
 };
