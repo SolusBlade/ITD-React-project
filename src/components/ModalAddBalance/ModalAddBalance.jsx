20679b49e981f43f1b1f8806b64e2b96de357041
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-04-19 23:18:30 +0300

    creates ModalAddBalance

---
+++ src/components/ModalAddBalance/ModalAddBalance.jsx
@@ -0,0 +1,42 @@
+import Icon from 'components/Icon/Icon';
+import s from './ModalAddBalance.module.scss';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const ModalAddBalance = ({ closeModal, text }) => {
+  return createPortal(
+    <div className={s.overlayAddIncome}>
+      <div className={s.modalWrapper}>
+        <form className={s.modalAddIncome}>
+          <input
+            className={s.modalInput}
+            type="text"
+            name="addSum"
+            placeholder={text}
+          />
+
+          <div className={s.btnContainer}>
+            <button className={s.btnAdd} type="submit">
+              Add
+            </button>
+            <button className={s.btnClose} type="button" onClick={closeModal}>
+              Cancel
+            </button>
+          </div>
+        </form>
+        <button className={s.btnSvg} onClick={closeModal}>
+          <Icon
+            name="icon-close"
+            width={24}
+            height={24}
+            className={s.iconClose}
+          />
+        </button>
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+export default ModalAddBalance;
