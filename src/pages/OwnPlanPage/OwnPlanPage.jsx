4bfe2115e15a07ee2cb67224c1f9204efec338c0
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-04-19 21:45:23 +0300

    creates background
    tablet and desktop

--- src/pages/OwnPlanPage/OwnPlanPage.jsx
+++ src/pages/OwnPlanPage/OwnPlanPage.jsx
@@ -1,10 +1,13 @@
 import Container from 'components/Container/Container';
 import PlanInputsList from 'components/PlanInputsList/PlanInputsList';
+import s from './OwnPlanPage.module.scss';
 
 const OwnPlanPage = () => {
   return (
     <Container>
       <PlanInputsList />
+
+      <div className={s.vectorImg} />
     </Container>
   );
 };
