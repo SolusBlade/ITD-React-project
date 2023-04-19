5cd0f0d8d3f26a93531c1c4a9711cc83d51e1f01
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-04-19 17:54:15 +0300

    Create optionsDefault.js

---
+++ src/data/optionsDefault.js
@@ -0,0 +1,40 @@
+const optionsDefault = [
+  {
+    title: 'RFP of both spouses, ₴',
+    name: 'salary',
+    type: 'text',
+    placeholder: '75 000',
+  },
+  {
+    title: 'Passive income, months, ₴',
+    name: 'passiveIncome',
+    type: 'text',
+    placeholder: 'Enter text',
+  },
+  {
+    title: 'Savings, ₴',
+    name: 'savings',
+    type: 'text',
+    placeholder: 'Enter text',
+  },
+  {
+    title: 'Specify the cost of your future apartment, ₴',
+    name: 'cost',
+    type: 'text',
+    placeholder: '90 000',
+  },
+  {
+    title: 'Specify the number of sq.m. of your future apartment',
+    name: 'footage',
+    type: 'text',
+    placeholder: 'Enter text',
+  },
+  {
+    title: 'Accumulation, %',
+    name: 'procent',
+    type: 'text',
+    placeholder: 'Enter text',
+  },
+];
+
+export default optionsDefault;
