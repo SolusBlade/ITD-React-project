61b2dbaa5e8a9159fd133af4d5c04935d57e5774
IhorC <igorchua@gmail.com>
2023-04-22 20:23:04 +0300

    outlet removed

--- src/pages/DynamicsPage/DynamicsPage.jsx
+++ src/pages/DynamicsPage/DynamicsPage.jsx
@@ -1,7 +1,6 @@
 import { Chart } from "components/Dynamics/Chart";
 import { Info } from "components/Dynamics/Info";
 import style from './DynamicsPage.module.scss';
-import { Outlet } from "react-router-dom";
 
 const DynamicsPage = () => {
   return <section className={style.section}>
