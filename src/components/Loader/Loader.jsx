c48f893dd36f3ad11ad6977bccd82d12b57a9183
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-04-22 22:29:19 +0300

    creates loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+import { RotatingLines } from 'react-loader-spinner';
+import s from './Loader.module.scss';
+
+const Loader = () => {
+  return (
+    <div className={s.backdrop}>
+      <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    </div>
+  );
+};
+
+export default Loader;
