faa4b1c0bd17740743d2dfad62e14e174b8baac4
Khashyrova <121459042+Khashyrova@users.noreply.github.com>
2023-04-23 16:04:14 +0200

    com

--- src/pages/StatisticsPage/Categories/Categories.jsx
+++ src/pages/StatisticsPage/Categories/Categories.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
 import d from './Categories.module.scss';
-import { categoryStatistic, getCashflowCategoriesPercentage } from 'redux/transactions/transactions-operations';
+import {
+  categoryStatistic,
+  getCashflowCategoriesPercentage,
+} from 'redux/transactions/transactionsOperations';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectIsLoading } from 'redux/transactions/transactions-selector';
+import { selectIsLoading } from 'redux/transactions/transactionsSelector';
 import { getCashflowCategoriesPercentageApi } from 'services/connectoinsApi';
 import { selectorIsLoggedIn } from 'redux/auth/authSelectors';
 
 const Categories = () => {
-
   const [categories, setCategories] = useState([]);
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectorIsLoggedIn);
@@ -15,45 +17,44 @@ const Categories = () => {
   useEffect(() => {
     const date = {
       year: new Date().getFullYear(),
-      month: new Date().getMonth()+1,
-    }
+      month: new Date().getMonth() + 1,
+    };
     console.log('lsplfkcps');
     isLoggedIn && getCashflowCategoriesPercentageApi(date);
-    
-  }, [isLoggedIn, dispatch])
-    
-    // const categories = [
-    //     {
-    //       category: 'products',
-    //       amount: 1000,
-    //       percentage: '25%',
-    //     },
-    //     {
-    //       category: 'product',
-    //       amount: 1000,
-    //       percentage: '25%',
-    //     },
-    //     {
-    //       category: 'produc',
-    //       amount: 1000,
-    //       percentage: '25%',
-    //     },
-    //     {
-    //       category: 'produ',
-    //       amount: 1000,
-    //       percentage: '25%',
-    //     },
-    //     {
-    //       category: 'prod',
-    //       amount: 1000,
-    //       percentage: '25%',
-    //     },
-    //     {
-    //       category: 'pro',
-    //       amount: 1000,
-    //       percentage: '25%',
-    //     },
-    //   ];
+  }, [isLoggedIn, dispatch]);
+
+  // const categories = [
+  //     {
+  //       category: 'products',
+  //       amount: 1000,
+  //       percentage: '25%',
+  //     },
+  //     {
+  //       category: 'product',
+  //       amount: 1000,
+  //       percentage: '25%',
+  //     },
+  //     {
+  //       category: 'produc',
+  //       amount: 1000,
+  //       percentage: '25%',
+  //     },
+  //     {
+  //       category: 'produ',
+  //       amount: 1000,
+  //       percentage: '25%',
+  //     },
+  //     {
+  //       category: 'prod',
+  //       amount: 1000,
+  //       percentage: '25%',
+  //     },
+  //     {
+  //       category: 'pro',
+  //       amount: 1000,
+  //       percentage: '25%',
+  //     },
+  //   ];
 
   return (
     <>
