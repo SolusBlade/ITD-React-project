0f1a61b13a22c97c3ab31c6cc9f0d816a92bc7ea
SolusBlade <kykysha59fafa@gmail.com>
2023-04-23 20:04:05 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,7 +14,6 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { ownPlanReducer } from './plan/planSlice';
-
 import  expensesReducer  from './expenses/expensesSlice';
 import { transactionsReducer } from './transactions/transactionsSlice';
 
