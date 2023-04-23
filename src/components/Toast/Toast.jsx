444b3ac1b03ce9e4a594d938982dbe66cb20a130
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-04-23 22:53:13 +0200

    adds Toast component for error notify

---
+++ src/components/Toast/Toast.jsx
@@ -0,0 +1,37 @@
+import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const notifyError = message =>
+  toast.error(message, {
+    position: 'top-center',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
+
+const Toast = () => {
+    return   <ToastContainer
+    position="top-center"
+    autoClose={3000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+    style={{
+      width: '700px',
+      height: '200px',
+      fontSize: '24px',
+      lineHeight: '36px',
+    }}
+  />
+  }
+  export default Toast
